e6fa5cc0f547dcb1148e6d33463db0bfbbdcd2bd
Y4Z <dev@y4z.dev>
2025-08-16 04:53:03 +0530

    updated: min sdk to andro 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.y4z.pixelfeatures"
-        minSdk = 36
+        minSdk = 34
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
